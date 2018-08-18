052e20c9dfd90e740abafb78b3875a4f8c5e5d21
Ashley <ashleyhellyer@gmail.com>
2018-08-18 13:01:35 -0400

    updated styles

---
+++ script.js
@@ -0,0 +1,391 @@
+
+const pepApp = {};
+
+pepApp.lastSentence = {
+    toughLove: {
+        nervous: [
+            'Just go do the damn thing and stop wasting time'
+        ],
+        mistake: [
+            'Now pick yourself up and move on'
+        ],
+        stupid: [
+            'Maybe you are stupid!'
+        ],
+        imposter: [
+            'Stop being a whiny baby and work hard'
+        ],
+        comparing: [
+            'Stop wasting so much time focussing on others!'
+        ],
+        gif: `src='styles/assets/toughLove.gif'`
+    },
+    tryYourBest: {
+        nervous: [
+            'Now get out there and be awesome!'
+        ],
+        mistake: [
+            'Keep your head up, tomorrow is a new day!'
+        ],
+        stupid: [
+            'Now take a walk and recharge your energy!'
+        ],
+        imposter: [
+            'You got this!'
+        ],
+        comparing: [
+            'Now get out there and try your best!'
+        ],
+        gif: `src='styles/assets/tryYourBest.gif'`
+    },
+    oprah: {
+        nervous: [
+            'final sentence oprah'
+        ],
+        mistake: [
+            'final sentence oprah'
+        ],
+        stupid: [
+            'final sentence oprah'
+        ],
+        imposter: [
+            'final sentence oprah'
+        ],
+        comparing: [
+            'final sentence oprah'
+        ],
+        gif: `src='styles/assets/oprah.gif'`
+    }
+}
+
+pepApp.pepTalks = {
+    toughLove: {
+        nervous: [
+            'Do. Or not do. There is no try. ', 
+            'If your dreams don\'t scare you they aren\'t big enough. ',
+            'You will never know your limits unless you push yourself to them. ',
+            'If it doesn\'t challenge you it doesn\'t change you. ',
+            'If it was easy everyone would do it. ',
+            'It\'s going to be hard, but hard is not impossible. ',
+            'Worrying is such a waste of time - it doesn\'t change anything. ',
+            'So you are nervous - are you going to sit inside and waste away your life by not taking any chances? '
+            ],
+
+        mistake: [
+            'The harder you fall the higher you bounce. ',
+            'It never gets easier, you just get better. ',
+            'If you think you are going to make it through life without making a mistake you are delusional. ',
+            'You are not a perfect snowflake that never does anything wrong. ',
+            'Learn to admit to your mistakes before someone exagerrates the story. ',
+            'You can learn a lot from your mistake when you aren\'t busy denying them. ',
+            'The winners are those who learn to take full responsibility for their actions. The losers are those who blame others for their failures. ',
+            'Don\'t confuse poor decision-making with destiny - own your mistakes. ',
+            'Your poor me attitude is annoying. Quit playing the victom and get over yourself. '
+            ],
+
+        stupid: [
+            'Winners train, losers complain. ',
+            'Hard work beats talent when talent doesn\'t work hard. ',
+            'It never gets easier, you just get better. ',
+            'Don\'t run away from challenges, run over them. ',
+            'Maybe if you tried reading something other than Instagram you would feel more intelligent. ',
+            'How hard did you even try? '
+            ],
+
+        imposter: [
+            'Snap out of this.Stop being defined by what other people think of you… or how they disappoint you.It’s lonely at the top — you know that. ', 
+            'I\'m not telling you it\'s going to be easy, I\'m telling you it\'s going to be worth it. ',
+            'Nothing is going to be handed to you on a silver platter. ',
+            'Life isn\'t going to give you contstant validation - you need to believe in yourself. '
+            ],
+
+        comparing: [
+            'Look in the mirror - that\'s your competition. ',
+            'Calling somebody else fat won\'t make you any skinner, calling someone else stupid won\'t make you any smarter. All you can do in life is to try and solve the problem that\'s in front of you. ',
+            'You will waste your life away if all you do is focus on other people. ',
+            'Sounds like somebody needs to take a social media break - get out of your head and starting living your own life. '
+
+            ],
+        },
+    tryYourBest: {
+        nervous: [
+            'A diamond is merely a lump of coal that did well under pressure! ',
+            'If you can dream it you can do it! ',
+            'Believe you can and you\'re halfway there! ',
+            'Repeat after me "I can do this"! ',
+            'If plan A doesn\'t work, the alphabet has 25 more letters! ',
+            'Start where you are, use what you have, do what you can! ',
+            'if you try your best there is no failure! ',
+            'Don\'t be afraid to fail, be afraid not to try! ',
+            'Don\'t try to be perfect, just do your best! '
+
+        ],
+            
+        mistake: [
+            'If plan A didn\t work, the alphabet has 25 more letters! ',
+            'When something goes wrong in your life just yell "plot twist" and move on! ',
+            'Failure is the condiment that gives success it\'s flavour! ',
+            'Life is a shipwreck but we must not forget to sing in the lifeboats! ',
+            'When life shuts a door, open it again. It\'s a door, that\'s how they work! ',
+            'You can\'t make everyone happy you are not a pizza! ',
+            'Make interesting, amazing, glorious fantastic mistakes! Leave the world more interesting for your being here! ',
+
+
+        ],
+
+        stupid: [
+            'Never let anyone treat you like a yellow startburst.You are a pink starburst! ',
+            'You are a magical unicorn! ',
+            'You never fail until you stop trying! '
+
+        ],
+
+        imposter: [
+            'Cupcakes are muffins that believed in miracles! ',
+            'If you were able to believe in santa claus for like 8 years, you can believe in yourself for like 5 minutes! ',
+            'You don\'t have to be great to start, but you have to start before you can be great! ',
+            'Just believe in yourself. Even if you don\'t, pretend that you do and at some point, you will! ',
+            'It\'s not who you are that holds you back, it\'s who you think you\'re not! ',
+            'Instead of why me, think why not me! '
+
+        ],
+
+        comparing: [
+            'We struggle with insecurity becuase we compare our behind-the - scenes with everyone else\'s highlight reel! ',
+            'Don\'t compare yourself to others. There\'s no comparison between the sun and the moon, they shine when it\'s their time! ',
+            'Prove yourself to yourself, not others! ',
+            'You can\'t actually be behind in life, there\'s no such thing! You\'re just doing your thing and they\'re just doing theirs! ',
+            'The only person you should compare yourself with is the person you used to be! ',
+            'A flower does not think of competing to the flower next to it. It just blooms! ',
+            'Normal is an illusion! What is normal for the spider is chaos for the fly! ',
+            'The fastest way to kill somethig special is to compare it to something else! ',        
+            'Don\'t compare your chapter 1 to someone else\'s chapter 20. '            
+        ]
+    },
+    oprah: {
+        nervous: [
+            'Breathe. Let go.And remind yourself that this very moment is the only one you know you have for sure. ',
+            'Every day brings a chance for you to draw in a breath, kick off your shoes, and dance. ',
+            'We can\'t become what we need to be by remaining what we are.',
+            'Doing your best at this moment puts you in the best place for the mext moment. ',
+            'You get in life what you have the courage to ask for.',
+            'This is your moment. Own it. ',
+            'Do the one thing you think you cannot do.Fail at it. Try again.Do better the second time. The only people who never tumble are those who never mount the high wire. '
+        ],
+            
+        mistake: [
+            'I trust that everything happens for a reason, even if we are not wise enough to see it. ',
+            'You get to know who you really are in a crisis. ',
+            'You are responsible for your life. You can\'t keep blaming somebody else for your dysfunction. Life is really about moving on. ',
+            'Failure is a great teacher and if you are open to it, every mistake has a lesson to offer. ',
+            'Failure is another stepping stone to greatness. ',
+            'Real integrity is going the right thing, knowing that nobody\'s going to know whether you did it or not. ',
+            'Whatever has happened to you in your past has no power over ths present moment because life is now. ',
+            'Every stumble is not a fall and every fall does not mean failure. ',
+            'Whenever I\’m faced with a difficult decision, I ask myself: What would I do if I weren\’t afraid of making a mistake, feeling rejected, looking foolish, or being alone ? I know for sure that when you remove the fear, the answer you’ve been searching for comes into focus. '
+
+        ],
+
+        stupid: [
+            'Where there is no struggle there is no strength. ',
+            'Think like a queen.A queen if not afraid to fail. Failure is another stepping stone to greatness. ',
+            'Do the one thing you think you cannot do. Fail at it. Try again. Do better the second time. The only people who never tumble are those who never mount the high wire. ',
+            'Failure is a great teacher and if you are open to it, every mistake has a lesson to offer. ',
+            'Go ahead, fall down. The world looks different from the ground. ',
+            'There is no such thing as failure. Failure is just life trying to move us in another direction. '
+        ],
+
+        imposter: [
+            'When you undervalue what you do, the world will undervalue who you are. ',
+            'Self - esteem comes from being able to define the world in your own terms and refusing to abide by the judgments of others. ',
+            'The greatest discovery of all time is that a person can change his future by merely changing his attitude. ',
+            'You get in life what you have the courage to ask for. ',
+            'The whole point of being alive is to evolve into the complete person you were intended to be. ',
+            'You don\'t become what you want, you become what you believe. '
+        ],
+
+        comparing: [
+            'The whole point of being alive is to evolve into the complete person you were intended to be. ',
+            'Be thankful for what you have; you\'ll end up having more. If you concentrate on what you don\'t have, you will never, ever have enough. ',
+            'Getting sucked into other people\'s ego,  dysfunction and darkness robs you of your own life. ',
+            'Why compare yourself with others ? No one in the entire world can do a better job of being you than you. ',
+            "Stop comparing yourself to other people. You're only on this planet to be you. " 
+        ],
+    }
+}
+
+pepApp.firstSentence = {
+    toughLove: {
+        nervous: (name) => {
+            return `Boo hoo, so you\'re nervous ${name}? `
+        },
+        mistake: (name) => {
+            return `So you made a mistake ${name}. `
+        },
+        stupid: (name) => {
+            return `Everyone feels stupid sometimes ${name}, what are you gonna do about it? `
+        },
+        imposter: (name) => {
+            return `What even is imposter syndrome - ugh ${name}. `
+        },
+        comparing: (name) => {
+            return `Why do you even care what anyone else is doing, ${name}? it\'s such a waste of time! `
+        }
+    },
+    tryYourBest: {
+        nervous: (name) => {
+            return `Don\'t be nervous ${name}! You have nothing to worry about! `
+        },
+        mistake: (name) => {
+            return `Everybody makes mistakes ${name}, you are human! `
+        },
+        stupid: (name) => {
+            return `You have the ability to learn and grow${name}, it just takes time! `
+        },
+        imposter:(name) => {
+            return `Story of my life, ${name}. We are in this together! `
+        },
+        comparing: (name) => {
+            return `${name}, you are a unique flower! `
+        }
+    },
+    oprah: {
+        nervous: (name) => {
+            return `${name}- Listen up. I\'ve learned a lot about being nervous over the years. `
+        },
+        mistake: (name) => {
+           return  `${name} - You will never grow as a person without a few mistakes. `
+        },
+        stupid:(name) =>  {
+            return `${name} - If I quit every time I felt stupid I wouldn\'t be here today. `
+        },
+        imposter: (name) => {
+            return `You need to reach into your soul, ${name}, and discover who you truly are. `
+        },
+        comparing: (name) => {
+            return `Listen up ${name}, comparison is the theft of joy. `
+        }
+    },
+
+}
+
+pepApp.getRandom = (array, finalArray) => {
+        let n = Math.floor(Math.random() * (array.length - 1));
+        finalArray.push(array.splice(n, 1));
+    
+}
+
+pepApp.getTalk = () => {
+    // listen for submit of form
+    // $('.user-name').on('submit', function (e) {
+    //     e.preventDefault();
+    $('.user-name-submit').on('click', function (e) {
+        e.preventDefault();
+       
+        //save user info:
+        pepApp.userName = $('#name').val();
+        $('.why-pep-talk').html(`Why do you need a pep talk ${pepApp.userName}<span class="accent-color">?</span>`)
+        $('.user-answers').toggleClass('hide show');
+        $('html, body').animate({
+            scrollTop: $(".user-answers").offset().top
+        }, 2000);
+        return false;
+    });
+    //listen for submit of pep talk style
+    $('.user-answers').on('submit', function (e) {
+        e.preventDefault();
+
+        pepApp.userReason = $('input[name=reason]:checked').val();
+        pepApp.userStyle = $('input[name=style]:checked').val();
+        
+        //array of choices
+        const allOptions = pepApp.pepTalks[pepApp.userStyle][pepApp.userReason];
+        //make copy of all options to send to randomize splice function
+        let copyAllOptions = allOptions.slice();
+        //create empty array to collect the final 3 sentences
+        let finalOptions = [];
+
+        for (i=0; i<3; i++) {
+        pepApp.getRandom(copyAllOptions, finalOptions);
+        }
+        let introSentence = pepApp.firstSentence[pepApp.userStyle][pepApp.userReason](pepApp.userName);
+        let pepParagraph = `
+                        <div class='pep-paragraph'>
+                            <p>${introSentence}</p>
+                            <p>${finalOptions[0]} ${finalOptions[1]}  ${finalOptions[2]}</p>
+                        </div>`
+
+        // $('.results').append(pepApp.firstSentence[pepApp.userStyle][pepApp.userReason](pepApp.userName));
+        // $('.results').append(finalOptions[0] + finalOptions[1] + finalOptions[2]);
+        $('.results').append(pepParagraph);
+        
+        const finalVideo = `<img ${pepApp.lastSentence[pepApp.userStyle].gif}>`
+
+        const button = `<a class='new-choice' href="#style-question">I need more!</a>`
+        $('.gif-container').append(finalVideo);
+        $('.results').append(button);
+        $('.results').toggleClass("hide show");
+
+    });
+
+    //listen for click on 
+    $('.results').on('click', '.new-choice', function() {
+        $('.gif-container, .pep-paragraph, .new-choice').empty();
+        console.log('click worked');
+        $('.style-question').html('Reee-do! What style do you want instead?');
+
+    });
+
+};
+
+// get random number whose max is two less than the length of an array
+pepApp.random = function (array) {
+    return Math.floor(Math.random() * (array.length - 2));
+};
+
+
+$(function () {
+    
+    pepApp.getTalk();
+    
+}); //end of doc ready
+
+
+
+
+
+
+//TO DO 
+
+//decide on last sentence
+//set up click event to shuffle
+//add auto scroll feature on click of submit
+//pick color scheme
+
+
+
+//ideas:
+//use css grid for layout
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
