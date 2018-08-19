ffd4ec14070573e39f447d4170b8aff1d74086ed
Ashley <ashleyhellyer@gmail.com>
2018-08-19 19:39:48 -0400

    added buttons to results section, finalized main styles

--- script.js
+++ script.js
@@ -75,12 +75,12 @@ pepApp.pepTalks = {
             'The harder you fall the higher you bounce. ',
             'It never gets easier, you just get better. ',
             'If you think you are going to make it through life without making a mistake you are delusional. ',
-            'You are not a perfect snowflake that never does anything wrong. ',
+            'You are not a perfect snowflake who never does anything wrong. ',
             'Learn to admit to your mistakes before someone exagerrates the story. ',
             'You can learn a lot from your mistake when you aren\'t busy denying them. ',
             'The winners are those who learn to take full responsibility for their actions. The losers are those who blame others for their failures. ',
             'Don\'t confuse poor decision-making with destiny - own your mistakes. ',
-            'Your poor me attitude is annoying. Quit playing the victom and get over yourself. '
+            'Your poor me attitude is annoying. Quit playing the victim and get over yourself. '
             ],
 
         stupid: [
@@ -93,7 +93,7 @@ pepApp.pepTalks = {
             ],
 
         imposter: [
-            'Snap out of this.Stop being defined by what other people think of you… or how they disappoint you.It’s lonely at the top — you know that. ', 
+            'Snap out of this. Stop being defined by what other people think of you… or how they disappoint you. It’s lonely at the top — you know that. ', 
             'I\'m not telling you it\'s going to be easy, I\'m telling you it\'s going to be worth it. ',
             'Nothing is going to be handed to you on a silver platter. ',
             'Life isn\'t going to give you contstant validation - you need to believe in yourself. '
@@ -130,18 +130,17 @@ pepApp.pepTalks = {
             'You can\'t make everyone happy you are not a pizza! ',
             'Make interesting, amazing, glorious fantastic mistakes! Leave the world more interesting for your being here! ',
 
-
         ],
 
         stupid: [
-            'Never let anyone treat you like a yellow startburst.You are a pink starburst! ',
+            'Never let anyone treat you like a yellow startburst. You are a pink starburst! ',
             'You are a magical unicorn! ',
             'You never fail until you stop trying! '
 
         ],
 
         imposter: [
-            'Cupcakes are muffins that believed in miracles! ',
+            'Did you know that cupcakes are muffins that believed in miracles? ',
             'If you were able to believe in santa claus for like 8 years, you can believe in yourself for like 5 minutes! ',
             'You don\'t have to be great to start, but you have to start before you can be great! ',
             'Just believe in yourself. Even if you don\'t, pretend that you do and at some point, you will! ',
@@ -151,23 +150,23 @@ pepApp.pepTalks = {
         ],
 
         comparing: [
-            'We struggle with insecurity becuase we compare our behind-the - scenes with everyone else\'s highlight reel! ',
+            'We struggle with insecurity because we compare our behind-the-scenes with everyone else\'s highlight reel! ',
             'Don\'t compare yourself to others. There\'s no comparison between the sun and the moon, they shine when it\'s their time! ',
             'Prove yourself to yourself, not others! ',
             'You can\'t actually be behind in life, there\'s no such thing! You\'re just doing your thing and they\'re just doing theirs! ',
             'The only person you should compare yourself with is the person you used to be! ',
             'A flower does not think of competing to the flower next to it. It just blooms! ',
             'Normal is an illusion! What is normal for the spider is chaos for the fly! ',
-            'The fastest way to kill somethig special is to compare it to something else! ',        
+            'The fastest way to kill something special is to compare it to something else! ',        
             'Don\'t compare your chapter 1 to someone else\'s chapter 20. '            
         ]
     },
     oprah: {
         nervous: [
-            'Breathe. Let go.And remind yourself that this very moment is the only one you know you have for sure. ',
+            'Breathe. Let go. And remind yourself that this very moment is the only one you know you have for sure. ',
             'Every day brings a chance for you to draw in a breath, kick off your shoes, and dance. ',
             'We can\'t become what we need to be by remaining what we are.',
-            'Doing your best at this moment puts you in the best place for the mext moment. ',
+            'Doing your best at this moment puts you in the best place for the next moment. ',
             'You get in life what you have the courage to ask for.',
             'This is your moment. Own it. ',
             'Do the one thing you think you cannot do.Fail at it. Try again.Do better the second time. The only people who never tumble are those who never mount the high wire. '
@@ -226,7 +225,7 @@ pepApp.firstSentence = {
             return `Everyone feels stupid sometimes ${name}, what are you gonna do about it? `
         },
         imposter: (name) => {
-            return `What even is imposter syndrome - ugh ${name}. `
+            return `What even is imposter syndrome - you are just like every other millenial, ${name}. `
         },
         comparing: (name) => {
             return `Why do you even care what anyone else is doing, ${name}? it\'s such a waste of time! `
@@ -240,10 +239,10 @@ pepApp.firstSentence = {
             return `Everybody makes mistakes ${name}, you are human! `
         },
         stupid: (name) => {
-            return `You have the ability to learn and grow${name}, it just takes time! `
+            return `You have the ability to learn and grow ${name}, it just takes time! `
         },
         imposter:(name) => {
-            return `Story of my life, ${name}. We are in this together! `
+            return `Imposter syndrome is the story of my life, ${name}. We are in this together! `
         },
         comparing: (name) => {
             return `${name}, you are a unique flower! `
@@ -251,13 +250,13 @@ pepApp.firstSentence = {
     },
     oprah: {
         nervous: (name) => {
-            return `${name}- Listen up. I\'ve learned a lot about being nervous over the years. `
+            return `${name}- listen up. I\'ve learned a lot about being nervous over the years. `
         },
         mistake: (name) => {
-           return  `${name} - You will never grow as a person without a few mistakes. `
+           return  `${name} - you will never grow as a person without a few mistakes. `
         },
         stupid:(name) =>  {
-            return `${name} - If I quit every time I felt stupid I wouldn\'t be here today. `
+            return `${name} - if I quit every time I felt stupid I wouldn\'t be here today. `
         },
         imposter: (name) => {
             return `You need to reach into your soul, ${name}, and discover who you truly are. `
@@ -323,38 +322,86 @@ pepApp.getTalk = () => {
         pepApp.getRandom(copyAllOptions, finalOptions);
         }
         let introSentence = pepApp.firstSentence[pepApp.userStyle][pepApp.userReason](pepApp.userName);
-        let pepParagraph = `
-                        <div class='pep-paragraph'>
-                            <p>${introSentence}</p>
+        let pepParagraph = 
+                            `<p>${introSentence}</p>
                             <p>${finalOptions[0]} ${finalOptions[1]}  ${finalOptions[2]}</p>
-                            <h3 class='new-choice' href = "#style-question" > I need more!</h3>
-                        </div>`
+                            <h3 class='new-choice' href = "#" > I still need more pep!</h3>
+                        `
+
+        let pepButtons = `<button class='reset-page'>Start from scratch</button>
+                          <button class='save-button'>Save my pep talk</button>`
 
         const finalVideo = `<img ${pepApp.lastSentence[pepApp.userStyle].gif}>`
 
         
 
         $('.user-style-form').toggleClass('show hide')
+        console.log('user style hidden');
         // $('.results').toggleClass('hide show');
-        $('.results').append(pepParagraph);
+        $('.pep-paragraph').append(pepParagraph);
+        
+        $('.results-wrapper').toggleClass('hide show')
+        console.log('wrapper shown');
         
         
-        // $('.gif-container').append(finalVideo);
-        // $('.results').append(button);
+        $('.gif-container').append(finalVideo);
+        $('.results-buttons').append(pepButtons);
         $('.results').toggleClass("hide show");
 
     });
 
-    //listen for click on 
+    //listen for click on new choice
     $('.results').on('click', '.new-choice', function() {
         $('.gif-container, .pep-paragraph, .new-choice').empty();
         console.log('click worked');
         $('.style-question').html('Reee-do! What style do you want instead?');
+        $('.results-wrapper').toggleClass('show hide')
         $('.results').toggleClass('show hide')
+        $('.results-buttons').empty();
         $('.user-style-form').toggleClass('hide show')
 
     });
 
+    //listen for click on reset  button
+    $('.results').on('click', '.reset-page', function() {
+        console.log('button was clicked')
+        //hard refresh page
+        location.reload(true);
+            
+
+    });
+
+    //listen for click on save button
+    $(".results").on('click', '.save-button', function () {
+        html2canvas($("#widget"), {
+            onrendered: function (canvas) {
+                theCanvas = canvas;
+                // document.body.appendChild(canvas);
+
+                canvas.toBlob(function (blob) {
+                    saveAs(blob, "PocketPepTalk.png");
+                });
+            }
+        });
+    });
+    //set favicon 
+    const favicon = document.querySelector("link[rel=icon]");
+
+    if (favicon) {
+        const emoji = favicon.getAttribute("data-emoji");
+
+        if (emoji) {
+            const canvas = document.createElement("canvas");
+            canvas.height = 64;
+            canvas.width = 64;
+
+            const ctx = canvas.getContext("2d");
+            ctx.font = "64px serif";
+            ctx.fillText(emoji, 0, 64);
+
+            favicon.href = canvas.toDataURL();
+        }
+    };
 };
 
 // get random number whose max is two less than the length of an array
@@ -367,6 +414,16 @@ $(function () {
     pepApp.getTalk();
     
 }); //end of doc ready
+
+
+
+
+
+
+
+
+
+
 
 
 
